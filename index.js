215837efb445a80abeb13318b14fbdbc9ff68dd7
Anish Shrestha <me.anish.shrestha@gmail.com>
2022-06-13 08:58:14 +0545

    ball color add

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 let environmentHeight = "400";
 let environmentWidth = "600";
 let timeDelay = 2;
+const colorArray = ["red", "blue", "green", "pink", "yellow", "orange"];
 
 function toPixel(number) {
   return `${number}px`;
 }
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function centerCalculation(obj) {
+  return { x: obj.x + obj.radius, y: obj.y + obj.radius };
+}
+
+function checkOverlap(obj1, obj2) {
+  let radiusDistance = obj1.radius + obj2.radius;
+  if (measureDistance(obj1, obj2) < radiusDistance * 0.9) {
+    obj1.x += radiusDistance * 0.1;
+    obj1.y += radiusDistance * 0.1;
+    obj2.x -= radiusDistance * 0.1;
+    obj2.y -= radiusDistance * 0.1;
+  }
 }
 
 const body = document.body;
@@ -28,15 +43,15 @@ function measureDistance(obj1, obj2) {
 
 class Ball {
   constructor(
+    color = "red",
     x = getRandomNumber(0, environmentWidth),
     y = getRandomNumber(0, environmentHeight),
-    w = getRandomNumber(20, 200),
+    w = getRandomNumber(10, 40),
     h = w,
-    sx = getRandomNumber(0.2, 2),
-    sy = getRandomNumber(0.2, 2),
+    sx = getRandomNumber(0.5, 1),
+    sy = getRandomNumber(0.5, 1),
     dx = 1,
-    dy = 1,
-    color = "red"
+    dy = 1
   ) {
     this.x = x;
     this.y = y;
@@ -81,25 +96,51 @@ class Ball {
   }
 
   ballCollisionCheck(obj1, obj2) {
+    let maxSpeed = 1;
+
     if (measureDistance(obj1, obj2) <= obj1.radius + obj2.radius) {
+      // let distance = measureDistance(obj1, obj2);
+      // let obj1Center = centerCalculation(obj1);
+      // let obj2Center = centerCalculation(obj2);
+      // let vCollision = {
+      //   x: obj2Center.x - obj1Center.x,
+      //   y: obj2Center.y - obj1Center.y,
+      // };
+      // let vCollisionNorm = {
+      //   x: vCollision.x / distance,
+      //   y: vCollision.y / distance,
+      // };
+      // let vRelativeVelocity = { x: obj1.sx - obj2.sx, y: obj1.sy - obj2.sy };
+      // let speed =
+      //   vRelativeVelocity.x * vCollisionNorm.x +
+      //   vRelativeVelocity.y +
+      //   vCollisionNorm.y;
+
+      checkOverlap(obj1, obj2);
+
+      // obj1.sx -= speed * vCollisionNorm.x;
+      // obj1.sy -= speed * vCollisionNorm.y;
+      // obj2.sx += speed * vCollisionNorm.x;
+      // obj2.sy += speed * vCollisionNorm.y;
+
+      // if (obj1.sx > maxSpeed) obj1.sx = maxSpeed;
+      // if (obj2.sx > maxSpeed) obj2.sx = maxSpeed;
+      // if (obj1.sy > maxSpeed) obj1.sy = maxSpeed;
+      // if (obj2.sy > maxSpeed) obj2.sy = maxSpeed;
+
       let temp = obj1.dx;
       obj1.dx = obj2.dx;
       obj2.dx = temp;
       temp = obj1.dy;
       obj1.dy = obj2.dy;
       obj2.dy = temp;
-      obj1.element.style.backgroundColor = "purple";
-      obj2.element.style.backgroundColor = "purple";
-    } else {
-      obj2.element.style.backgroundColor = "red";
-      obj1.element.style.backgroundColor = "red";
     }
   }
 }
-const BALL_NUMBER = 3;
+const BALL_NUMBER = 100;
 let ballArrays = [];
 for (let i = 0; i < BALL_NUMBER; i++) {
-  ballArrays.push(new Ball());
+  ballArrays.push(new Ball(colorArray[i % 6]));
 }
 
 function play() {
